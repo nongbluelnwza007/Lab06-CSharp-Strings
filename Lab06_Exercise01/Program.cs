3eba665bb9b7f35e8746fb2451c977e106e43abe
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-24 23:23:31 +0700

    add

---
+++ Lab06_Exercise01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Hello, I'm 66030010");
+System.Console.WriteLine("Please enter your name");
+var name = System.Console.ReadLine();  
+System.Console.WriteLine($"Your name is {name}");
---
+++ Lab06_Exercise03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s1 = "A string is more ";
+string s2 = "than the sum of its chars.";
+   
+System.Console.WriteLine("Before Concatenate of s1 and s2");
+System.Console.WriteLine($"s1 = \"{s1}\", Hash code = {s1.GetHashCode():X}");
+System.Console.WriteLine($"s2 = \"{s2}\", Hash code = {s2.GetHashCode():X}");
+
+   
+s1 += s2;
+   
+System.Console.WriteLine("After Concatenate of s1 and s2");
+System.Console.WriteLine($"s1 = \"{s1}\", Hash code = {s1.GetHashCode():X}");
+System.Console.WriteLine($"s2 = \"{s2}\", Hash code = {s2.GetHashCode():X}");

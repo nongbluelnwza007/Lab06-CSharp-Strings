c51d266944c0200440219a3d38a8f95b4fdeab3b
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-29 22:26:34 +0700

    add

--- Lab06_Exercise05/obj/Debug/net8.0/Lab06_Exercise05.AssemblyInfo.cs
+++ Lab06_Exercise05/obj/Debug/net8.0/Lab06_Exercise05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab06_Exercise05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ad4789dc9c4b8c7591e09ddf98bf977159e50d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eba665bb9b7f35e8746fb2451c977e106e43abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab06_Exercise05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab06_Exercise05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
